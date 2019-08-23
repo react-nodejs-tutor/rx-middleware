4b3cad2426624f2b32ce755e19e399da39024bf1
koojaa <93jwkoo@gmail.com>
2019-08-23 22:18:46 +0900

    redux middleware tuts' template

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { changeInput, insert } from './store/modules/form';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	handleChange = e => {
+		const { value } = e.target;
+		this.props.changeInput(value);
+	};
+
+	handleSubmit = e => {
+		e.preventDefault();
+
+		const { changeInput, insert, input } = this.props;
+
+		insert(input);
+		changeInput('');
+	};
+
+	render() {
+		const { input, list } = this.props;
+
+		return (
+			<div>
+				<form action="">
+					<input value={input} onChange={this.handleChange} />
+					<button onClick={this.handleSubmit}>입력</button>
+				</form>
+				{list.map(item => {
+					return <div key={item.id}>{item.text}</div>;
+				})}
+			</div>
+		);
+	}
 }
 
-export default App;
+export default connect(
+	({ form: { input, list } }) => ({
+		input,
+		list
+	}),
+	{
+		changeInput,
+		insert
+	}
+)(App);
